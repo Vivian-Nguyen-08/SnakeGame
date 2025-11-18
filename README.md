ede63fc780a4c4656e91405fd01cd5f92a04ce7e
Vivian Nguyen <nguyeviv@Vivians-MacBook-Air-2.local>
2025-11-18 10:52:58 -0600

    Coded the Neural Network and QTrainer located in model.py

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SnakeGame
+The files classifier.py, layers.py, optim.py are used to code model.py without needing to use pytorch. 
+Inside of model.py there are two classes: Linear_QNet (neural network) and QTrainer (trainer class that performs the Q-Learning Update rule)
+
+Linear_QNet 
+- Implements a fully connected neural network with one hidden layer. The parameters are initialized internally already, so the constructor can be called without arguments. 
+- Handles forward pass computations (used to predict Q-values) and backward pass (used to compute gradients for weights, bias, activations).
+- Provides a save() and load() file so the model can be stored in a .pkl file and restored later.
+
+QTrainer 
+- Converts the current state and next state into NumPy arrays and feeds them through the model to predict the Q-values. 
+- Uses the Bellman Equation to calculate the target Q-value based on the action taken and the current state: 
+    Qnew = reward[idx] + self.gamma * np.max(next_q)
+    - reward[idx] is the reward received for this experience.
+    - gamma determines how much the agent values future rewards compared to immediate rewards.
+    - np.max(next_q) looks at all possible actions in the next state and chooses the one with the highest predicted reward.
+- Compute the MSE by measuring the difference between the Target Q-value and Predicted Q-value. 
+- Performs a backward pass and applies gradient descent to update model's weights. 
+
+
+
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from layers import fc_forward, fc_backward, relu_forward,relu_backward
 from optim import SGD
 
 class Linear_QNet(Classifier): 
-    def __init__(self, input_size,hidden_size,output_size):
+    def __init__(self, input_size=11, hidden_size=256, output_size=3):
         """
         Initialize a new two layer network.
 
@@ -60,4 +60,63 @@ class Linear_QNet(Classifier):
         q_values,_ = self.forward(state)
         return q_values
     
+    def save(self, file_name='model.pkl'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_path = os.path.join(model_folder_path, file_name)
+        super().save(file_path)
     
+    @classmethod
+    def load(cls,file_name='model.pkl'): 
+        model_folder_path = './model'
+        file_path = os.path.join(model_folder_path, file_name)
+        model = super(Linear_QNet, cls).load(file_path)
+        return model 
+    
+
+class QTrainer: 
+    def __init__(self,model,lr,gamma): 
+        self.lr = lr 
+        self.gamma = gamma 
+        self.model = model 
+        self.optimizer = SGD(model.parameters(),learning_rate=lr)
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = np.array(state)
+        next_state = np.array(next_state)
+        action = np.array(action)
+        reward = np.array(reward)
+
+        if len(state.shape) == 1:
+            state = state.reshape(1, -1)
+            next_state = next_state.reshape(1, -1)
+            action = action.reshape(1, -1)
+            reward = reward.reshape(1, -1)
+            done = [done]
+
+        # predict Q values with current state 
+        pred, cache = self.model.forward(state)
+
+        target = pred.copy()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                next_q, _ = self.model.forward(next_state[idx:idx+1])
+                # bellman equation 
+                Q_new = reward[idx] + self.gamma * np.max(next_q)
+            
+            target[idx][np.argmax(action[idx])] = Q_new
+    
+        loss, grad_pred = self.mse_loss(pred, target)
+        grads = self.model.backward(grad_pred, cache)
+        self.optimizer.step(grads)
+        
+        return loss 
+    
+    def mse_loss(self,pred,target): 
+        loss = np.sum((pred - target) ** 2) / (2 * pred.shape[0])
+        grad = (pred - target) / (pred.shape[0])
+
+        return loss,grad 
